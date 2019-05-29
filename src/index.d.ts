a2fae627e040d9d38c92f25f2bf4d6067f5ddb18
Anton Gilgur <agilgur5@gmail.com>
2019-05-28 22:40:52 -0400

    (format/types): use standard style for types
    - no semicolons, no newline at the top
    - also change props link to be on one line -- it's important enough
      that I don't think it should be in parens

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,25 +1,23 @@
+import * as React from 'react'
 
-import * as React from 'react';
-
-// signature_pad's props 
-// (taken from https://github.com/szimek/signature_pad/blob/e2af461bca7ceb7cc5afb8349930908ca19f2f56/src/signature_pad.ts#L23)
+// signature_pad's props from https://github.com/szimek/signature_pad/blob/e2af461bca7ceb7cc5afb8349930908ca19f2f56/src/signature_pad.ts#L23
 export interface IOptions {
-    dotSize?: number | (() => number);
-    minWidth?: number;
-    maxWidth?: number;
-    minDistance?: number;
-    backgroundColor?: string;
-    penColor?: string;
-    throttle?: number;
-    velocityFilterWeight?: number;
-    onBegin?: (event: MouseEvent | Touch) => void;
-    onEnd?: (event: MouseEvent | Touch) => void;
+  dotSize?: number | (() => number),
+  minWidth?: number,
+  maxWidth?: number,
+  minDistance?: number,
+  backgroundColor?: string,
+  penColor?: string,
+  throttle?: number,
+  velocityFilterWeight?: number,
+  onBegin?: (event: MouseEvent | Touch) => void,
+  onEnd?: (event: MouseEvent | Touch) => void,
 }
 
 // props specific to the React wrapper
 export interface SignatureCanvasProps extends IOptions {
-    canvasProps?: any;
-    clearOnResize?: boolean;
+  canvasProps?: any,
+  clearOnResize?: boolean
 }
 
 export default class SignatureCanvas extends React.Component<SignatureCanvasProps> {}
